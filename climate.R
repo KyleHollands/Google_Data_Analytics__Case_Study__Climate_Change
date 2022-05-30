bb5aa43ba3ea43d7532f97ad404b94d11c3072b8
Kyle Hollands <kylehollands@gmail.com>
2022-05-29 21:05:49 -0400

    Updated climate.R

--- climate.R
+++ climate.R
@@ -12,57 +12,71 @@ library(directlabels)
 options("scipen"=10)
 
 # Import the dataset
-climate_change_metrics <- read.csv("./Datasets/Original CSV/GlobalLandTemperaturesByCity.csv")
+climate_data <- read.csv("./Datasets/Original CSV/GlobalLandTemperaturesByCity.csv")
 
 # Inspect the dataset
-colnames(climate_change_metrics)
-glimpse(climate_change_metrics)
-nrow(climate_change_metrics)
-dim(climate_change_metrics)
-head(climate_change_metrics)
-tail(climate_change_metrics)
-summary(climate_change_metrics)
-str(climate_change_metrics)
-View(climate_change_metrics)
+colnames(climate_data)
+glimpse(climate_data)
+nrow(climate_data)
+dim(climate_data)
+head(climate_data)
+tail(climate_data)
+summary(climate_data)
+str(climate_data)
+View(climate_data)
 
 # Check for NA's
-sapply(climate_change_metrics, function(x) sum(is.na(x)))
+sapply(climate_data, function(x) sum(is.na(x)))
 
 # Drop rows with NA's - In this case, the rows with NA's don't contain any other important metrics. 
-climate_change_metrics <- na.omit(climate_change_metrics)
+climate_data <- na.omit(climate_data)
+sapply(climate_data, function(x) sum(is.na(x)))
 
 # Convert the date column from character to datetime values.
-climate_change_metrics <- climate_change_metrics %>% 
+climate_data <- climate_data %>% 
   mutate(dt = ymd(dt))
 
 # Rename some columns
-names(climate_change_metrics) <- c("Date", "AverageTemperature", "AverageTemperatureUncertainty", "City", "Country", "Latitude", "Longitude")
+names(climate_data) <- c("Date", "AvgTemperature", "AvgTemperatureUncertainty", "City", "Country", "Latitude", "Longitude")
 
-test <- climate_change_metrics %>% 
-    group_by(Country, year = lubridate::floor_date(Date, "year")) %>%
-    summarize(AvgTemperature = mean(AverageTemperature), AvgTemperatureUncertainty = mean(AverageTemperatureUncertainty))
+# Condense the data to only years.
+climate_data_condensed <- climate_data %>% 
+    group_by(Country, Year = lubridate::floor_date(Date, "year")) %>%
+    summarize(AvgTemperature = mean(AvgTemperature), AvgTemperatureUncertainty = mean(AvgTemperatureUncertainty))
 
-glimpse(test)
-View(test)
+glimpse(climate_data_condensed)
 
-# Calculate the difference between the max temperature recorded and the average temperature (by Country)
-climate_change_metrics %>%
+View(climate_data_condensed)
+head(climate_data_condensed)
+tail(climate_data_condensed)
+
+# Acquire only the average temperature per year, for all Countries.
+climate_data_condensed_two <- climate_data_condensed %>%
+  group_by(Year) %>%
+  summarize(AvgTemperature = mean(AvgTemperature), AvgTemperatureUncertainty = mean(AvgTemperatureUncertainty))
+
+View(climate_data_condensed_two)
+
+# Calculate the difference between the max AVG temperature recorded per year and the average temperature (by Country)
+(climate_data_condensed %>%
   group_by(Country) %>%
-  summarize(max(AverageTemperature - mean(AverageTemperature)))
+  summarize(AvgTemperature = mean(AvgTemperature), AvgTemperatureUncertainty = mean(AvgTemperatureUncertainty)))
 
-climate_change_metrics %>%
-  group_by(Country) %>%
-  summarize(max(AverageTemperatureUncertainty - mean(AverageTemperatureUncertainty)))
+(climate_data_condensed_two %>%
+  group_by(Year) %>%
+  summarize(AvgTemperature = mean(AvgTemperature), AvgTemperatureUncertainty = mean(AvgTemperatureUncertainty)))
 
 # Look at which Countries have seen the largest increases.
-largest_increase <- climate_change_metrics %>%
-  group_by(Country, Date) %>%
-  summarize(max(Average Temperature - mean(Average Temperature)))
+largest_increases <- climate_data_condensed %>%
+  group_by(Country) %>%
+  summarize(LargestIncreases = max(AvgTemperature - mean(AvgTemperature)), LargestUncertainties = max(AvgTemperatureUncertainty - mean(AvgTemperatureUncertainty))) %>%
+  filter(LargestIncreases > 3)
 
-colnames(largest_increase)
-glimpse(largest_increase)
+colnames(largest_increases)
+glimpse(largest_increases)
+View(largest_increases)
 
-ggplot(climate_change_metrics, aes(x = Date, y = AverageTemperature)) +
-  geom_point() +
-  geom_smooth(span = 0.3)
-
+ggplot(climate_data_condensed_two,
+       aes(x = Year)) +
+  geom_line(aes(y = AvgTemperature), color = "red") +
+  geom_line(aes(y = AvgTemperatureUncertainty), color = "green", linetype = "twodash")
